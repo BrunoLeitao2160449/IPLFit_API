8496c56e22e8d02bfa1921e1293314f079e54c3d
BrunoLeitao2160449 <2160449@my.ipleiria.pt>
2017-11-14 16:41:31 +0000

    details

--- config/web.php
+++ config/web.php
@@ -43,14 +43,16 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                [
+                    'class' => 'yii\rest\UrlRule',
+                    'controller' => 'clientes'
+                ],
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
